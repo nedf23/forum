23e6af5a848235af6272fb403f88f57351801120
Ned Fenstermacher <nedfenstermacher@gmail.com>
2017-03-16 23:04:49 -0400

    Initial commit after first three lessons.

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    public function testAUserCanBrowseThreads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    public function testAUserCanReadASingleThread()
+    {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    public function testAUserCanReadRepliesThatAreAssociatedWithAThread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}
