c08c5fbe88a68e527500bc19689006638b3aac26
vagrant <vagrant@homestead>
2017-04-03 22:36:10 +0000

    Lesson 16.

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -63,7 +63,10 @@ class ThreadController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        return view('threads.show', compact('thread'));
+        return view('threads.show', [
+            'thread' => $thread,
+            'replies' => $thread->replies()->paginate(5),
+        ]);
     }
 
     /**
